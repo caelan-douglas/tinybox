75d9ab62855e13c0171462ab034099e361b52bc7
Caelan Douglas <caelan.douglas@outlook.com>
2024-07-20 23:58:43 -0400

    Update exporter

--- exporter.sh
+++ exporter.sh
@@ -6,14 +6,18 @@ rm -rf ../tinybox_latest/*
 
 mkdir ../tinybox_latest
 mkdir ../tinybox_latest/binaries
+mkdir ../tinybox_latest/binaries/Tinybox-linux
+mkdir ../tinybox_latest/binaries/Tinybox-mac
+mkdir ../tinybox_latest/binaries/Tinybox-win
 
-godot --headless --export-release "linux" ../tinybox_latest/binaries/Tinybox.x86_64
-godot --headless --export-release "mac" ../tinybox_latest/binaries/Tinybox.app
-godot --headless --export-release "win" ../tinybox_latest/binaries/Tinybox.exe
+
+godot --headless --export-release "linux" ../tinybox_latest/binaries/Tinybox-linux/Tinybox.x86_64
+godot --headless --export-release "mac" ../tinybox_latest/binaries/Tinybox-mac/Tinybox.app
+godot --headless --export-release "win" ../tinybox_latest/binaries/Tinybox-win/Tinybox.exe
 
 echo '\n\nCompressing builds, please wait.'
 
 # Finder compress tool
-ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox.x86_64 ../tinybox_latest/Tinybox-$VERNAME-linux.zip
-ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox.app ../tinybox_latest/Tinybox-$VERNAME-mac.zip
-ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox.exe ../tinybox_latest/Tinybox-$VERNAME-win.zip
+ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox-linux ../tinybox_latest/Tinybox-$VERNAME-linux.zip
+ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox-mac ../tinybox_latest/Tinybox-$VERNAME-mac.zip
+ditto -c -k --sequesterRsrc --keepParent ../tinybox_latest/binaries/Tinybox-win ../tinybox_latest/Tinybox-$VERNAME-win.zip
