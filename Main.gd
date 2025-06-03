d1dd783375cf5ab60e3a8e9e822abf56e355cff9
Caelan Douglas <caelan.douglas@outlook.com>
2025-06-03 19:23:56 -0400

    Use FastLZ compression for network

--- Main.gd
+++ Main.gd
@@ -23,6 +23,7 @@ const PLAYER : PackedScene = preload("res://data/scene/character/RigidPlayer.tsc
 const CAMERA : PackedScene = preload("res://data/scene/camera/Camera.tscn")
 const PORT = 30815
 const SERVER_INFO_PORT = 30816
+const NETWORK_COMPRESSION_MODE := ENetConnection.CompressionMode.COMPRESS_FASTLZ
 # thread for UPNP connection
 var thread : Thread = null
 var upnp : UPNP = null
@@ -233,6 +234,7 @@ func _on_host_pressed() -> void:
 			return
 	# Get the host's selected map from the dropdown.
 	# Create the server.
+	enet_peer.host.compress(NETWORK_COMPRESSION_MODE)
 	enet_peer.create_server(PORT)
 	# Set the current multiplayer peer to the server.
 	multiplayer.multiplayer_peer = enet_peer
@@ -311,6 +313,7 @@ func _on_join_pressed(address : Variant = null, is_from_list := false) -> void:
 	join_button.text = JsonHandler.find_entry_in_file("ui/join_clicked")
 	
 	# Create the client.
+	enet_peer.host.compress(NETWORK_COMPRESSION_MODE)
 	enet_peer.create_client(str(address), PORT)
 	# Set the current multiplayer peer to the client.
 	multiplayer.multiplayer_peer = enet_peer
