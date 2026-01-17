a4709e096b1fd439e4d1c81472ba137eeadfa087
Carmen <carmen.lamprecht1@gmail.com>
2026-01-17 15:33:33 -0500

    debug

--- src/Magnet.gd
+++ src/Magnet.gd
@@ -1,5 +1,5 @@
 # Tinybox
-# Copyright (C) 2023-present Caelan Douglas
+# Copyright (C) 2023-present Caelan Douglas Carmen Lamprecht
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -19,6 +19,7 @@ class_name Magnet
 
 var north_pos : Vector3 = Vector3.ZERO
 var south_pos : Vector3 = Vector3.ZERO
+
 @onready var magnet_area : Area3D = $MagnetDetectRegion
 
 # Set a custom property
@@ -44,10 +45,11 @@ func set_colour(new : Color) -> void:
 
 func _init() -> void:
 	_brick_spawnable_type = "brick_magnet"
-	properties_to_save = ["global_position", "global_rotation", "brick_scale", "immovable", "indestructible", "magnetic_field",]
+	properties_to_save = ["global_position", "global_rotation", "brick_scale", "immovable", "indestructible",]
 
 func _ready() -> void:
 	super()
+	self.name = "magnet"
 	await get_tree().create_timer(0.5).timeout
 	unfreeze_entire_group()
 
@@ -58,23 +60,38 @@ func sync_properties(props : Dictionary) -> void:
 func enter_state() -> void:
 	super()
 
+func _on_sleeping_state_changed() -> void:
+	# If we are sleeping, and we are NOT glued, and we are NOT in a group (eg. lone brick)
+	pass
+		
 func _physics_process(delta : float) -> void:
-	super(delta)
+	#super(delta)
 	var magnet_area_others : = magnet_area.get_overlapping_bodies()
 	for other_magnet in magnet_area_others:
-		if other_magnet is not Magnet or other_magnet == self:
+		#var a: string = other_magnet.w
+		print(other_magnet.name)
+		if "magnet" not in other_magnet.name or other_magnet == self:
 			return
 			
-		north_pos = transform.basis.y * brick_scale.y/2 #up vector relative to brick * 
-		south_pos = - north_pos
+		north_pos = transform.basis.y * (brick_scale.y+1)/2 #up vector relative to brick * 
+		south_pos = - transform.basis.y * (brick_scale.y-1)/2
 		
 		var distance : float = (other_magnet.global_position - global_position).length()
-		var force_magnitude : float = clampf(1 / distance, 0.001, 1)
 		
-		var northA_northB : Vector3 = 1000* force_magnitude * (other_magnet.north_pos - north_pos)
-		var northA_southB : Vector3 = 1000* force_magnitude * (other_magnet.north_pos - south_pos)
-		var southA_northB : Vector3 = 1000* force_magnitude * (other_magnet.south_pos - north_pos)
-		var southA_southB : Vector3 = 1000* force_magnitude * (other_magnet.south_pos - south_pos)
+		var aaa : float = other_magnet.north_pos.dot(north_pos)
+		var bbb : float = other_magnet.north_pos.dot(south_pos)
+		var ccc : float = other_magnet.south_pos.dot(north_pos)
+		var ddd : float = other_magnet.south_pos.dot(south_pos)
+		
+		var force_magnitude1 : float = clampf(1 / aaa, 0.001, 1)
+		var force_magnitude2 : float = clampf(1 / bbb, 0.001, 1)
+		var force_magnitude3 : float = clampf(1 / ccc, 0.001, 1)
+		var force_magnitude4 : float = clampf(1 / ddd, 0.001, 1)
+		
+		var northA_northB : Vector3 = 1000* force_magnitude1 * (other_magnet.north_pos - north_pos)
+		var northA_southB : Vector3 = 1000* force_magnitude2 * (other_magnet.north_pos - south_pos)
+		var southA_northB : Vector3 = 1000* force_magnitude3 * (other_magnet.south_pos - north_pos)
+		var southA_southB : Vector3 = 1000* force_magnitude4 * (other_magnet.south_pos - south_pos)
 		
 		apply_force(northA_northB, north_pos)
 		#apply_force(northA_southB, north_pos)
